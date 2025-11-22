fa1ff713a07ed0b16e4da3b4e76fe6f86dda76d3
DoanCongPho <congpho115@gmail.com>
2025-11-22 21:02:09 +0700

    Downgrade Django and Click for Python 3.9 support

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,14 @@
-# Ensure pip is available and updated
+# build_files.sh
+echo "Building..."
+
+# Ensure pip is updated
 python3.9 -m ensurepip
 python3.9 -m pip install --upgrade pip
 
-# Install dependencies
+# Install the fixed requirements
 python3.9 -m pip install -r requirements.txt
 
 # Collect static files
 python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build End"
